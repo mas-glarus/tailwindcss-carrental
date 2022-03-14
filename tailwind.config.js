8d2a716352f175ce54824b9ceac8e1595b99ee72
marc aschwanden <marc.aschwanden88@gmail.com>
2022-03-14 20:32:23 +0100

    insert texts

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,22 @@ module.exports = {
       orange1: 	'#e38826',
       cyan1: '#006970',
       cyan2: '#004241',
+      col1: '#ffffff',
+      col2: '#f2f2f2',
       },
+      fontFamily: {
+        deca: ['"Lexend Deca"'],
+        shoulders: ['"Big Shoulders Display"'],
+        },
+      backgroundImage: {
+        sedan: "url(/C:/Users/Marc/Documents/1_WebDev/9_webdev/HTML_CSS/tailwind/carrental/images/icon-sedans.svg)",
+      },
+      width: {
+        wmax: '51.5rem',
+      },
+      height: {
+        hmax: '37.5rem',
+      }
     },
   },
   plugins: [],
