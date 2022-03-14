8410cded73585fe16e91173300874a255d51bda6
marc aschwanden <marc.aschwanden88@gmail.com>
2022-03-14 17:25:26 +0100

    open project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {    
+      colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      orange1: 	'#e38826',
+      cyan1: '#006970',
+      cyan2: '#004241',
+      },
+    },
+  },
+  plugins: [],
+}
